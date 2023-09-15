a0760010be85dd1dd8dd80e595de060a3bcee71c
pta tien <anhtien372002@gmail.com>
2023-09-15 18:49:09 +0700

    upload files

---
+++ src/app.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, Request, UploadFile, File
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+
+IMAGEDIR = "./images/"
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+app.mount("/images", StaticFiles(directory="images"), name="images")
+
+
+@app.get('/', response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.post("/upload-files")
+async def create_upload_files(request: Request, file: UploadFile = File(...)):
+    contents = await file.read()
+
+    # save the file
+    with open(f"{IMAGEDIR}{file.filename}", "wb") as f:
+        f.write(contents)
+
+    show = file.filename
+    return templates.TemplateResponse("index.html", {"request": request, "show": show})
